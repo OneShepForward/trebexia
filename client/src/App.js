089650114cbd0009525016bcba0d5d7b5792736d
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-01-24 18:31:02 -0500

    added css components
    added Button, Game, Start components and updated package-lock with react-router-dom and styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Start from './Start';
+import Game from './Game';
+
+// import {
+//   BrowserRouter,
+//   Switch,
+//   Route,
+//   Link,
+// } from "react-router-dom";
 
 function App() {
+ 
+  const [start, setStart] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz">
+      { start ? <Game /> : <Start props={setStart} />} 
     </div>
   );
 }
---
+++ client/src/Game.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Game () {
+
+return (
+    <div>
+        <h2>Question 1 of 7</h2>
+    </div>
+)
+}
+
+export default Game;
