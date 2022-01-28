d31f23baa2a4c108df544fe59b9b1006d599be6c
Nicholas Shephard <shephardn@gmail.com>
2022-01-28 13:40:39 -0600

    scores populating

--- client/src/Scores.js
+++ client/src/Scores.js
@@ -3,14 +3,14 @@ import './styles/App.css';
 import ScoreCard from "./ScoreCard";
 
 function Scores() {
-    const [questions, setQuestions] = useState([])
+    const [scores, setScores] = useState([])
 
     useEffect(() => {
-        fetch("https://morganick.herokuapp.com/game_to_render/1").then((response) => {
+        fetch("https://morganick.herokuapp.com/scores").then((response) => {
           if (response.ok) {
-            response.json().then((question_array) => {
-            //   console.log(question_array)
-                setQuestions(question_array)
+            response.json().then((scores) => {
+              console.log(scores)
+                setScores(scores)
             });
           } else {
             response.json().then((error) => console.log(error))
@@ -18,19 +18,20 @@ function Scores() {
         });
       }, []);
 
-    function renderQuestions() {
-        // console.log("renderQuestion hit", questions)
-        return Object.values(questions).map(q => 
-            <p>{q.question}</p>)
-    }
+    function renderScores() {
+        // console.log("renderQuestion hit", scores)
+        return (
+        <ol>
+          {scores.map(s => <li>{s.user.username} - {s.score}</li>)}
+        </ol>
+        )}
       
 
     return (
-        <div>
-            {renderQuestions()}
-            <h2>Scores</h2>
-
-        </div>
+      <div className = "scores-list">
+        <h2>Top Scores</h2>
+          {renderScores()}
+      </div>
     )
 }
 
