232e1945629c6fe1eebb2594edcffd49516277c0
Nicholas Shephard <shephardn@gmail.com>
2022-03-09 10:04:47 -0600

    Updated config

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#logout"
 
   # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
